74f70ccc9f98e406853bd6f82850612430cf9fb0
Aidan Gannon <aidanwilliamgannon@gmail.com>
2025-11-23 13:43:06 +0000

    Add function seperation with keymaps

--- nvim/.config/nvim/lua/plugins/filetree.lua
+++ nvim/.config/nvim/lua/plugins/filetree.lua
@@ -1,8 +1,10 @@
+local function keymaps()
+    require("nvim-tree").setup()
+    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+    vim.keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>")
+end
+
 return {
     "nvim-tree/nvim-tree.lua",
-    config = function()
-        require("nvim-tree").setup()
-        vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
-        vim.keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>")
-    end
+    config = keymaps
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,19 @@
+local function keymaps()
+    vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          vim.keymap.set("n", 'gd', vim.lsp.buf.definition, { buffer = args.buf })
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = args.buf })
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = args.buf })
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { buffer = args.buf })
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { buffer = args.buf })
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { buffer = args.buf })
+        end
+    })
+end
+
+keymaps()
+
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -10,18 +26,6 @@ return {
             ensure_installed = { "ts_ls", "pyright" }
         })
         
-        vim.api.nvim_create_autocmd("LspAttach", {
-            callback = function(args)
-              vim.keymap.set("n", 'gd', vim.lsp.buf.definition, { buffer = args.buf })
-              vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
-              vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = args.buf })
-              vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = args.buf })
-              vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { buffer = args.buf })
-              vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { buffer = args.buf })
-              vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { buffer = args.buf })
-            end
-        })
-
         vim.lsp.enable("ts_ls")
         vim.lsp.enable("pyright")
     end
