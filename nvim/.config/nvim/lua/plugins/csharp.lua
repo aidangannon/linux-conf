5389398bde0f84d7013078bc26f81ce76f922f46
Aidan <aidanwilliamgannon@gmail.com>
2025-11-25 15:31:20 +0000

    Add more roslyn

--- nvim/.config/nvim/lua/plugins/csharp.lua
+++ nvim/.config/nvim/lua/plugins/csharp.lua
@@ -1,5 +1,3 @@
-vim.keymap.set("n", "<leader>rr", ":Roslyn restart<CR>", { desc = "Restart Roslyn" })
-
 vim.api.nvim_create_user_command("CSharpSetup", function()
     vim.cmd("MasonInstall roslyn csharpier")
 end, { desc = "Install C# tools" })
