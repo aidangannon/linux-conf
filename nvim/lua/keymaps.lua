1e3570dbf2ca15b936943abd5aa8a537e203e8ea
aidangannon <aidanwilliamgannon@gmail.com>
2026-01-24 22:42:20 +0000

    Add some emacs limiting

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -11,10 +11,10 @@ vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" 
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 
-vim.keymap.set({'n', 'v'}, '<C-b>', 'h')
-vim.keymap.set({'n', 'v'}, '<C-f>', 'l')
-vim.keymap.set({'n', 'v'}, '<C-p>', 'k')
-vim.keymap.set({'n', 'v'}, '<C-n>', 'j')
+vim.keymap.set({'n', 'v'}, '<Left>', 'h')
+vim.keymap.set({'n', 'v'}, '<Right>', 'l')
+vim.keymap.set({'n', 'v'}, '<Up>', 'k')
+vim.keymap.set({'n', 'v'}, '<Down>', 'j')
 
 vim.keymap.set({'n', 'v'}, 'h', '<Nop>')
 vim.keymap.set({'n', 'v'}, 'j', '<Nop>')
